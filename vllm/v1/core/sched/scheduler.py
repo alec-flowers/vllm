c605800b0f5e3a1c57d4f0ab47df46e997502c59
alec-flowers <aflowers@nvidia.com>
2025-12-02 09:36:01 -0800

    relax assert to handle race condition
    Signed-off-by: alec-flowers <aflowers@nvidia.com>

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -1581,7 +1581,13 @@ class Scheduler(SchedulerInterface):
             self.finished_recving_kv_req_ids.add(req_id)
         for req_id in kv_connector_output.finished_sending or ():
             logger.debug("Finished sending KV transfer for request %s", req_id)
-            assert req_id in self.requests
+            if req_id not in self.requests:
+                logger.warning(
+                    "Got finished sending KV transfer for request %s,"
+                    "but the request is already freed.",
+                    req_id,
+                )
+                continue
             self._free_blocks(self.requests[req_id])
 
     def _update_requests_with_invalid_blocks(
